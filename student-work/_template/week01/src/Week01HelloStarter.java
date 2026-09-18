dcb3ace8e502735f2ac5803221378073d524a451
TomHerbertSax <35017156+TomHerbertSax@users.noreply.github.com>
2026-09-18 01:01:20 +0000

    Complete Week 01 work

--- student-work/_template/week01/src/Week01HelloStarter.java
+++ student-work/_template/week01/src/Week01HelloStarter.java
@@ -1,7 +1,12 @@
 public class Week01HelloStarter {
 
     public static void main(String[] args) {
-        System.out.println("TODO: replace this with your name.");
+      /*  System.out.println("TODO: replace this with your name.");
+        System.out.println("TODO: add your course goal.");
+        System.out.println("TODO: add one more line about yourself.");
+    */
+
+        System.out.println("Tom Herbert.");
         System.out.println("TODO: add your course goal.");
         System.out.println("TODO: add one more line about yourself.");
     }
